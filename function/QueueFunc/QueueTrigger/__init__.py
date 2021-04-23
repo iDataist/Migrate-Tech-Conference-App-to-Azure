b055fe1536900632ccf6ef42ed09685e06d9ec11
Hui Ren <huiren16@gmail.com>
2021-04-23 12:41:55 -0500

    Update __init__.py

--- function/QueueFunc/QueueTrigger/__init__.py
+++ function/QueueFunc/QueueTrigger/__init__.py
@@ -1,4 +1,3 @@
-export PATH=~/anaconda3/bin:$PATH
 import logging
 import azure.functions as func
 import psycopg2
@@ -6,7 +5,6 @@ import os
 from datetime import datetime
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
-from datetime import date
 
 def main(msg: func.ServiceBusMessage):
 
@@ -14,15 +12,21 @@ def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
 
     # Get connection to database
-    conn = psycopg2.connect(dbname="techconfdb", user="dbadmin@sqlserver20210405", password="p@ssword1234", host="sqlserver20210405.postgres.database.azure.com")
+    conn = psycopg2.connect(dbname="techconfdb", 
+                            user="dbadmin@sqlserver20210405", 
+                            password="p@ssword1234", 
+                            host="sqlserver20210405.postgres.database.azure.com")
     cur = conn.cursor()
     try:
         # Get notification message and subject from database using the notification_id
-        subject = cur.execute("SELECT subject FROM notification WHERE id={};".format(notification_id))
-        body = cur.execute("SELECT message FROM notification WHERE id={};".format(notification_id))
+        cur.execute("SELECT subject, message FROM notification WHERE id={};".format(notification_id))
+        result = cur.fetchall()
+        subject, body = result[0][0], result[0][1]
 
         # Get attendees email and name
-        attendees = cur.execute("SELECT email, first_name FROM attendee;").fetchall()
+        cur.execute("SELECT email, first_name FROM attendee;")
+        attendees = cur.fetchall()
+
         # Loop through each attendee and send an email with a personalized subject
         num_attendee_notified = 0
         for (email, first_name) in attendees:
@@ -34,15 +38,17 @@ def main(msg: func.ServiceBusMessage):
             try:
                 sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
                 response = sg.send(mail)
-                if response.status_code == "200":
+                if response.status_code == "202":
                     num_attendee_notified += 1
             except Exception as e:
                 print(e.message)
-        status = "{} of attendees notified".format(num_attendee_notified)
+        status = "Notified {} attendees".format(num_attendee_notified)
         # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
-        cur.execute("UPDATE notification SET status = {}, completed_data = {} WHERE id = {};".format(status, date.today(), notification_id))
+        cur.execute("UPDATE notification SET status = '{}', completed_date = '{}' WHERE id = {};".format(status, datetime.utcnow(), notification_id))
+        conn.commit()
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
+        conn.rollback()       
     finally:
         # Close connection
         cur.close()
