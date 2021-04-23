76f8f33cd07cda0c23a41aa2f7a4b30b834df5d2
Hui Ren <huiren16@gmail.com>
2021-04-23 14:28:06 -0500

    Update config.py

--- web/config.py
+++ web/config.py
@@ -12,7 +12,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or DB_URL
     CONFERENCE_ID = 1
     SECRET_KEY = 'LWd2tzlprdGHCIPHTd4tp5SBFgDszm'
-    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://servicebus20210405.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=dArGgVHewR1ZdVX+B4rbfA5eEFFUQm21MlPkWWWkOeI=' 
+    SERVICE_BUS_CONNECTION_STRING ='Endpoint=sb://servicebus20210405.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=GwQOg4Wnsl+9PL5IqQkyefMRZJGjn7BOsiERhOsa95w=' 
     SERVICE_BUS_QUEUE_NAME ='notificationqueue'
     ADMIN_EMAIL_ADDRESS: 'info@techconf.com'
     SENDGRID_API_KEY = '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' #Configuration not required, required SendGrid Account
