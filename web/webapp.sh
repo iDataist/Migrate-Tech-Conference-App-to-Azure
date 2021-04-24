6041dcbd556fcf3ea781df6c79a953b4d0113ff5
Hui Ren <huiren16@gmail.com>
2021-04-24 12:43:42 -0500

    Update webapp.sh

--- web/webapp.sh
+++ web/webapp.sh
@@ -2,23 +2,23 @@ uniqueId=20210405
 resourceGroup="group$uniqueId"
 location='westus2'
 appServicePlan="appservice$uniqueId"
-webApp="webapp$uniqueId"
+webApp="techconf2021"
 
-# Create an app service plan
-az appservice plan create --name $appServicePlan \
-                          --resource-group $resourceGroup \
-                          --sku F1
+# # Create an app service plan
+# az appservice plan create --name $appServicePlan \
+#                           --resource-group $resourceGroup \
+#                           --is-linux \
+#                           --sku F1
 
 # Create a web app
 az webapp create --name $webApp \
                  --plan $appServicePlan \
                  --resource-group $resourceGroup \
-                 --runtime python
+                 --runtime "PYTHON|3.7"
 
 # Deploy the web app
 az webapp up \
     --resource-group $resourceGroup\
     --name $webApp \
     --sku F1 \
-    --verbose \
-    --html       
+    --verbose    
